7a2bbfb0ca25f6ff0812ba3297eed8db6aae3139
Zacc <lhsrebel72@gmail.com>
2021-11-22 19:05:40 -0600

    original branch commit

--- src/client/Main.js
+++ src/client/Main.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import CartView from './Views/Cart';
 import HomeView from './Views/Home';
-import ProductView from './Views/Products';
+import ProductsView from './Views/Products';
 
 const Main = () => {
   return (
     <Routes> {/* The Switch decides which component to show based on the current URL.*/}
       <Route exact path='/' element={<HomeView/>}></Route>
-      <Route exact path='/products' element={<ProductView/>}></Route>
+      <Route exact path='/products' element={<ProductsView/>}></Route>
       <Route exact path='/cart' element={<CartView/>}></Route>
     </Routes>
   );
